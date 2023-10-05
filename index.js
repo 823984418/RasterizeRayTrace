ad7d8f4eceadfb85b14db59bbeaf4bdefe5897d4
823984418 <823984418@qq.com>
2023-10-05 23:44:48 +0800

    Revert "测试修复Firefox Android缺少clearBuffer实现"
    This reverts commit 7a51f3a9ebab873f3caef916b80f04ba27d7de28.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import {Renderer, RendererConfig} from "./renderer.js";
 import {mat3, mat4, vec3, vec4} from "./gl-matrix/index.js";
+import {loadPmxTextureModel} from "./load_pmx_model.js";
+import {mat4FromMat3} from "./kits.js";
 import {loadObjLightModelWithoutNormal, loadObjStaticModelWithoutNormal} from "./load_obj_model.js";
 
 
@@ -33,39 +35,27 @@ let device = await adapter.requestDevice({
     label: "renderer",
 });
 
-if (GPUCommandEncoder.prototype.clearBuffer == null || true) {
-    let zeroBuffer = device.createBuffer({
-        size: 1024 * 1024 * 16,
-        usage: GPUBufferUsage.COPY_SRC,
-    });
-    GPUCommandEncoder.prototype.clearBuffer = function (buffer, offset, size) {
-        offset ??= 0;
-        size ??= buffer.size - offset;
-        this.copyBufferToBuffer(zeroBuffer, 0, buffer, offset, size);
-    }
-}
-
 adapter.requestAdapterInfo().then(info => {
     document.querySelector("#gpuInfo").innerText = `${info.vendor} : ${info.architecture} : ${info.description}`;
 });
 
 let config = new RendererConfig();
-config.renderWidth = 1024;
-config.renderHeight = 1024;
+config.renderWidth = 512;
+config.renderHeight = 512;
 config.composeWidth = config.renderWidth;
 config.composeHeight = config.renderHeight;
-config.traceMappingSize = 800;
-config.traceWordSize = 800;
+config.traceMappingSize = 500;
+config.traceWordSize = 500;
 config.shadowTextureSize = 256;
 config.lightTextureSize = 128;
-config.traceCount = 2;
-config.traceDepth = 2;
+config.traceCount = 40;
+config.traceDepth = 1;
 config.lightSampleCount = 1;
 config.shadowNearDistance = 10;
 config.shadowFarDistance = 900;
 config.traceDepthBias = 0.005;
 config.shadowDepthBias = 0.001;
-config.debug_taa = false;
+config.debug_taa = true;
 config.taa_factor = 0.93;
 config.taa_maxDeltaZ = 0.005;
 config.taa_fastPower = 1 / 2;
@@ -124,27 +114,19 @@ vec4.copy(right.modelInfo.diffuse.buffer, [0.14, 0.45, 0.091, 1]);
 mat4.copy(right.modelInfo.model.buffer, boxMatrix);
 renderer.models.push(right);
 
-// let bunny = loadObjStaticModelWithoutNormal(renderer, await (await fetch("models/bunny/bunny.obj")).text());
-// vec4.copy(bunny.modelInfo.emit.buffer, [0.6, 0.6, 0.6, 1]);
-// vec4.copy(bunny.modelInfo.diffuse.buffer, [0.9, 0.9, 0.9, 1]);
-// let bunnyMatrix = mat4.create();
-// mat4.translate(bunnyMatrix, bunnyMatrix, [375, 152, 250]);
-// mat4.scale(bunnyMatrix, bunnyMatrix, [800, 800, 800]);
-// mat4.rotateY(bunnyMatrix, bunnyMatrix, Math.PI);
-// mat4.copy(bunny.modelInfo.model.buffer, bunnyMatrix);
-// mat4.copy(bunny.modelInfo.normalModel.buffer, mat4FromMat3(mat3.normalFromMat4([], bunnyMatrix)));
-// renderer.models.push(bunny);
-//
-// let pmx = await loadPmxTextureModel(renderer, new URL("models/ningguang/凝光.pmx", document.baseURI));
+let bunny = loadObjStaticModelWithoutNormal(renderer, await (await fetch("models/bunny/bunny.obj")).text());
+vec4.copy(bunny.modelInfo.emit.buffer, [0.6, 0.6, 0.6, 1]);
+vec4.copy(bunny.modelInfo.diffuse.buffer, [0.9, 0.9, 0.9, 1]);
+let bunnyMatrix = mat4.create();
+mat4.translate(bunnyMatrix, bunnyMatrix, [375, 152, 250]);
+mat4.scale(bunnyMatrix, bunnyMatrix, [800, 800, 800]);
+mat4.rotateY(bunnyMatrix, bunnyMatrix, Math.PI);
+mat4.copy(bunny.modelInfo.model.buffer, bunnyMatrix);
+mat4.copy(bunny.modelInfo.normalModel.buffer, mat4FromMat3(mat3.normalFromMat4([], bunnyMatrix)));
+renderer.models.push(bunny);
 
+let pmx = await loadPmxTextureModel(renderer, new URL("models/ningguang/凝光.pmx", document.baseURI));
 
-let shortbox = loadObjStaticModelWithoutNormal(renderer, await (await fetch("models/cornellbox/shortbox.obj")).text());
-vec4.copy(shortbox.modelInfo.diffuse.buffer, [0.725, 0.71, 0.68, 1]);
-renderer.models.push(shortbox);
-
-let tallbox = loadObjStaticModelWithoutNormal(renderer, await (await fetch("models/cornellbox/tallbox.obj")).text());
-vec4.copy(tallbox.modelInfo.diffuse.buffer, [0.725, 0.71, 0.68, 1]);
-renderer.models.push(tallbox);
 
 let x = 0;
 let y = 0;
@@ -158,15 +140,19 @@ if (config.debug_taa) {
     mat4.rotateY(matrix, matrix, (performance.now() - begin) * 0.0001);
 }
 
-// pmx.forEach(model => {
-//     mat4.copy(model.modelInfo.model.buffer, matrix);
-//     mat4.copy(model.modelInfo.normalModel.buffer, mat4FromMat3(mat3.normalFromMat4([], matrix)));
-// });
+pmx.forEach(model => {
+    mat4.copy(model.modelInfo.model.buffer, matrix);
+    mat4.copy(model.modelInfo.normalModel.buffer, mat4FromMat3(mat3.normalFromMat4([], matrix)));
+});
 
 function updateCamera() {
 
-    let eye = vec3.add([], [278, 273, 300], vec3.rotateY([], vec3.rotateX([], [0, 0, -1100], [0, 0, 0], y * 4), [0, 0, 0], -x * 4));
-
+    let eye;
+    if (config.debug_taa) {
+        eye = vec3.add([], [278, 273, 300], vec3.rotateY([], vec3.rotateX([], [0, 0, -1100], [0, 0, 0], 0), [0, 0, 0], 0));
+    } else {
+        eye = vec3.add([], [278, 273, 300], vec3.rotateY([], vec3.rotateX([], [0, 0, -1100], [0, 0, 0], y * 4), [0, 0, 0], -x * 4));
+    }
     renderer.setCamera(eye, [278, 273, 300], [0, 1, 0], Math.PI * 0.22, canvas.clientWidth / canvas.clientHeight, 500, 2000);
     if (!config.debug_taa) {
         renderer.beginTime = performance.now();
@@ -192,27 +178,27 @@ renderer.onRenderListeners.push(() => {
             mat4.rotateY(matrix, matrix, (performance.now() - begin) * 0.0001);
         }
         let nm = mat3.normalFromMat4([], matrix);
-        // pmx.forEach(model => {
-        //     mat4.copy(model.modelInfo.model.buffer, matrix);
-        //     let nn = model.modelInfo.normalModel.buffer;
-        //     nn[0] = nm[0];
-        //     nn[1] = nm[1];
-        //     nn[2] = nm[2];
-        //     nn[3] = 0;
-        //     nn[4] = nm[3];
-        //     nn[5] = nm[4];
-        //     nn[6] = nm[5];
-        //     nn[7] = 0;
-        //     nn[8] = nm[6];
-        //     nn[9] = nm[7];
-        //     nn[10] = nm[8];
-        //     nn[11] = 0;
-        //     nn[12] = 0;
-        //     nn[13] = 0;
-        //     nn[14] = 0;
-        //     nn[15] = 0;
-        //
-        // });
+        pmx.forEach(model => {
+            mat4.copy(model.modelInfo.model.buffer, matrix);
+            let nn = model.modelInfo.normalModel.buffer;
+            nn[0] = nm[0];
+            nn[1] = nm[1];
+            nn[2] = nm[2];
+            nn[3] = 0;
+            nn[4] = nm[3];
+            nn[5] = nm[4];
+            nn[6] = nm[5];
+            nn[7] = 0;
+            nn[8] = nm[6];
+            nn[9] = nm[7];
+            nn[10] = nm[8];
+            nn[11] = 0;
+            nn[12] = 0;
+            nn[13] = 0;
+            nn[14] = 0;
+            nn[15] = 0;
+
+        });
     }
 
 
